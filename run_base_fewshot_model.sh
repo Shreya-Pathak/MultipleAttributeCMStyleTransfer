a5c1b041cfd0e1e3965429bffa5e8c2b92c69496
Sneha Mondal <snehamondal@google.com>
2022-05-15 04:38:27 +0000

    initial code

--- run_base_fewshot_model.sh
+++ run_base_fewshot_model.sh
@@ -7,23 +7,23 @@ python train_mt5_base_fewshot.py \
 --per_device_train_batch_size=8 \
 --per_device_eval_batch_size=8 \
 --gradient_accumulation_steps=2 \
---overwrite_output_dir=True \
+--overwrite_output_dir=False \
 --predict_with_generate \
 --train_file='./data/few_shot_training/base_train.tsv' \
 --validation_file='./data/few_shot_training/base_dev.tsv' \
 --test_file=./data/few_shot_training/base_test.tsv \
 --load_best_model_at_end \
---metric_for_best_model='loss' \
---num_train_epochs=1.0 \
+--metric_for_best_model='bleu' \
+--num_train_epochs=30.0 \
 --learning_rate=5e-4 \
---eval_steps=10 \
---save_steps=10 \
+--eval_steps=500 \
+--save_steps=500 \
 --evaluation_strategy='steps' \
 --save_strategy='steps' \
+--save_total_limit=2 \
 --lr_scheduler_type='constant' \
 --generation_num_beams=1 \
 --generation_max_length=128 \
 --optim='adafactor' \
 --max_source_length=128 \
---max_target_length=128 \
---max_train_samples=100
+--max_target_length=128
