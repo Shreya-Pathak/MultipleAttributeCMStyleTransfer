51ed7660dd1f0408249dd9bc578dee3b2d25fbf3
Sneha Mondal <sneha.12392@gmail.com>
2022-08-26 04:13:06 +0530

    Update metrics.py
    Computing metrics on actual generated text.

--- metrics.py
+++ metrics.py
@@ -47,8 +47,9 @@ def compute_metrics(eval_preds, tokenizer, data_args):
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
     # Some simple post-processing
-    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
-
+    decoded_preds_raw, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+	decoded_preds = decoded_preds_raw.split('|||')[1].strip()
+	
     result = bleu(decoded_labels, decoded_preds)
 #    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
     result = {"bleu": result["bleu"]}
